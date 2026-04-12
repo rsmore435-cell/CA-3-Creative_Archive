74affaa0a5fc6ef19767f119736b29c9713193cf
Rohan More <rsmore435@gmail.com>
2026-04-12 18:20:56 +0530

    Backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,10 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.json());
 
 // Test route
@@ -21,6 +24,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB Connected ✅'))
   .catch(err => console.log(err));
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT} 🚀`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT} 🚀`);
 });
