fd7f9871902ea36362904df6efedec299b1bf1f7
Rohan More <rsmore435@gmail.com>
2026-04-14 17:07:10 +0530

    added articles option

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   title:      { type: String, required: true },
   content:    { type: String, required: true },
-  type:       { type: String, enum: ['poetry', 'lyrics'], required: true },
+  type:       { type: String, enum: ['poetry', 'lyrics', 'articles'], required: true },
   genre:      { type: String },
   author:     { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   authorName: { type: String }
